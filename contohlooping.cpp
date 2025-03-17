efc23683263696dc977f7c5e3195f78cb1fe5ed0
rossakyl <rossakayla01@gmail.com>
2025-03-17 20:09:37 +0700

    mencetak nilai counter

--- contohlooping.cpp
+++ contohlooping.cpp
@@ -8,3 +8,12 @@ int main(){
     int i;
     int perulanganWhile = 0;
     int perulanganDo = 0;
+    
+    //Loop with for
+    //Loop for ascending
+    for(counter = 0; counter < 5; counter++){
+        cout << "Nilai counter = " << counter << " TI UMY" << endl;
+    }
+    cout << "Nilai counter terakhir = " << counter << endl;
+    cout << endl;
+
