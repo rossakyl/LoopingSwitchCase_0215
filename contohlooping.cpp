ee97bf7313841fea92efabcccbe9e9d1b21bcfc9
rossakyl <rossakayla01@gmail.com>
2025-03-17 19:15:24 +0700

    add library

---
+++ contohlooping.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+    int counter;
+    int i;
+    int perulanganWhile = 0;
+    int perulanganDo = 0;
+
+    //Loop with for
+    //Loop for ascending
+    for(counter = 0; counter < 5; counter++){
+        cout << "Nilai counter = " << counter << " TI UMY" << endl;
+    }
+    cout << "Nilai counter terakhir = " << counter << endl;
+    cout << endl;
+    
+} 
